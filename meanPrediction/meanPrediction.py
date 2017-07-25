9c932a80fa711b8dff0b4a78eefa29500c607f84
Roberto <fellin.roberto@hotmail.it>
2017-07-25 16:17:40 +0200

    meanPrediction

--- meanPrediction/meanPrediction.py
+++ meanPrediction/meanPrediction.py
@@ -1,1 +1,17 @@
-a
+import csv
+
+sum=[0,0,0,0,0]
+c=0
+with open('../../personality/big5.sorted.csv') as fin:
+    headerline = fin.next()
+    total = 0
+
+    for row in csv.reader(fin):
+        c+=1
+        for x in range(1, 6):
+            print row[x]
+            sum[x-1] = round(sum[x-1] + float(row[x]),2)
+
+
+sum=[x/c for x in sum]
+print sum
