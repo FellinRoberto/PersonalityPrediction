9afdf1190dc6ae68789d80405a1cb9b54053966b
Roberto <fellin.roberto@hotmail.it>
2017-07-27 09:44:24 +0200

    fix meanPredicton

--- meanPrediction/meanPrediction.py
+++ meanPrediction/meanPrediction.py
@@ -1,35 +1,32 @@
 import csv
-from sklearn.metrics import mean_squared_error
+import pandas as pd
 
 sum=[0,0,0,0,0]
 c=0
 
 #read the input and do the sum of the parameter of all the user, output array with size 5 with the sum of the parameter
-with open('../../personality/big5.sorted2.csv') as fin:
+with open('../dataset/y_train.csv') as fin:
 
     headerline = fin.next()
     total = 0
 
     for row in csv.reader(fin):
         c+=1
-        for x in range(1, 6):
-            print row[x]
+        for x in range(0, 5):
             sum[x-1] = round(sum[x-1] + float(row[x]),2)
 
 
 # do the mean, dividing the sum for the number of users
 sum=[x/c for x in sum]
-print sum
-
-#read the parameter and use it as input
-y_true = [[0.5, 1, 0.5, 1, 1],[-1, 1, 0.5, 1, 1],[7, -6, 0.5, 1, 1]]
+print "prediction: ",sum
 
 #compute the prediction as the mean previous calculated
 y_pred=[]
+users=pd.read_csv('../dataset/X_test.csv', engine='c')
 
-for x in range(1, 4):
+for x in range(1, len(users)):
     y_pred.append (sum)
 
-print y_true
-print y_pred
-print mean_squared_error(y_true, y_pred)
+y_pred=pd.DataFrame(y_pred)
+y_pred.to_csv('y_pred.csv', index=False, quoting=csv.QUOTE_NONNUMERIC, doublequote=True)
+print "y_pred.csv gererated"
