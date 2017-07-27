50ceeea0172436023864216416b115ba31ceeead
chinidamiano <chinidamiano@gmail.com>
2017-07-27 16:52:01 +0200

    fix all columns of personality in bow regression

--- bow/bow_predictor.py
+++ bow/bow_predictor.py
@@ -12,7 +12,7 @@ n_features = 5000  # numero massimo di parole prese in considerazione in ogni po
 
 tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=0.01, max_features=n_features, stop_words='english')
 
-print x_train
+#print x_train
 bow_train = tfidf_vectorizer.fit_transform(x_train['status_update'])
 
 bow_train = bow_train.todense()
@@ -22,20 +22,27 @@ np.savetxt('tfidfTrain.csv', bow_train, delimiter=',')
 bow_test = tfidf_vectorizer.transform(x_test['status_update'])
 bow_test = bow_test.todense()
 np.savetxt('tfidfTest.csv', bow_test, delimiter=',')
+
 # tfidf_df = pd.DataFrame(tfidf.toarray())
 # tfidf_df.to_csv('tfidf.csv', index=False, quoting=csv.QUOTE_NONNUMERIC, doublequote=True)
 
-param_dist = {'objective': 'reg:linear', 'n_estimators': 2}
-
-clf = xgb.XGBRegressor(**param_dist)
-
 y_train = y_train.as_matrix()
-y_train = y_train[:, 0]
 
-clf.fit(bow_train, y_train, eval_metric='logloss', verbose=True)
+n=5
 
-prediction = clf.predict(bow_test)
-print prediction
-print prediction.shape
+prediction = np.empty(shape=[len(x_test), 0])
+
+
+for x in range(0, n):
+	clf=xgb.XGBRegressor(learning_rate = 0.1, n_estimators=1000,
+                           max_depth=5, min_child_weight=1,
+                           gamma=0, subsample=0.8,
+                           colsample_bytree=0.8, objective= "reg:linear",  
+                           nthread=-1,scale_pos_weight=1, seed=27)
+	y_t = y_train[:, x]
+	clf.fit(bow_train, y_t)
+	temp =  clf.predict(bow_test)
+	prediction = np.column_stack((prediction, temp))
+	print x
 
 np.savetxt('bowPrediction.csv', prediction, delimiter=',')
---
+++ evaluator/evaluator_bow.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+predictions=pd.read_csv('../bow/bowPrediction.csv', engine='c',header=None)
+true_values = pd.read_csv('../dataset/y_test.csv', engine='c')
+
+print len(predictions)
+print len(true_values)
+
+print mean_squared_error(true_values, predictions)
